5599e54b3ab8a3ecc111dc13057f5260c77540a0
Hatefulpegasus <glenashaffer@gmail.com>
2020-06-10 15:51:04 -0400

    Revert "Fist full version"
    This reverts commit 771f65943ef8092063de5b0e287301690269bc89.

--- PuzzleGame/ElectroTrap.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-
-func _on_ElectroTrap_body_entered(body):
-	if body.get_groups().has("Player") :
-		get_tree().call_group("Player", "die")
--- PuzzleGame/StartMenu.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-signal start_game
-
-func _ready():
-	$StartContainer/Button.connect("pressed", self, "start_button_press")
-
-func start_button_press():
-	emit_signal("start_game")
